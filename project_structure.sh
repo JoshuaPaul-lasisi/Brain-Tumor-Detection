8865fd7dba422ab8b54147f38e85c5c7c49e927f
Tobi Joshua <jpaullasisi@gmail.com>
2024-11-13 01:31:44 +0100

    rewrote project structure

--- project_structure.sh
+++ project_structure.sh
@@ -1,9 +1,12 @@
-project/
+BRAIN-TUMOR-DETECTION/
 ├── data/
+│   ├── processed/                   # processed dataset
 │   ├── Training/                    # Training dataset
 │   └── Testing/                     # Testing dataset
 ├── model/
 │   └── brain_tumor_model.keras      # Saved model file
+├── notebooks/
+│   └── notebook.ipynb
 ├── reports/
 │   ├── images/                      # Folder for storing plots/images
 │   └── summary.md                   # Summary markdown file
@@ -11,7 +14,17 @@ project/
 │   ├── data_preprocessing.py        # Script for data loading and preprocessing
 │   ├── model_building.py            # Script for building and training the model
 │   ├── utils.py                     # Script for plotting utility functions
-│   └── app.py                       # Flask app
+│   └── flask_app.py                       # Flask app
+│   └── streamlit_app.py                       # Streamlit app
+├── static/
+│   ├── css/
+│       └── styles.css
+│   ├── js/
+│       └── scripts.js
+├── templates/
+│   ├── index.html
 ├── Dockerfile                       # Dockerfile for creating the container
+├── LICENSE 
+├── project_structure.sh 
 ├── requirements.txt                 # Required packages
 └── README.md                        # README file for the project
